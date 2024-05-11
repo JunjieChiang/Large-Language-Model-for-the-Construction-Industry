43a0a34a00bc8089cb2eccdd6fca851dcd9c0716
Chengzi <1304317752@qq.com>
2024-05-11 13:54:11 +0800

    Revert "0511_Chengzi"
    This reverts commit fbc5b3eb4eb640c0856cc721f6606eace2d2eba5.

--- embedding.py
+++ embedding.py
@@ -1,13 +1,6 @@
 from FlagEmbedding import FlagModel
 import faiss
 import logging
-import numpy as np
-
-def load_embeddings(load_path):
-    return np.load(load_path)
-
-def save_embeddings(embeddings, save_path):
-    np.save(save_path, embeddings)
 
 
 def setup_logging():
@@ -15,16 +8,14 @@ def setup_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def load_embedding_model(model_path, dimension, data_path ):
+def load_embedding_model(model_path, dimension, data_path):
 
     logging.info("Loading model and encoding data...")
     model = FlagModel(model_path,
                       query_instruction_for_retrieval="为这个JSON数据生成表示以用于检索相关属性：",
                       use_fp16=True) # Setting use_fp16 to True speeds up computation with a slight performance degradation
     sentences = load_sentences(data_path)
-    #embeddings = model.encode(sentences).astype('float32')
-    #save_embeddings(embeddings, '/content/RE-Generate/example/corpus_embeddings.npy')
-    embeddings = load_embeddings('example/corpus_embeddings.npy')
+    embeddings = model.encode(sentences).astype('float32')
     index = faiss.IndexFlatL2(dimension)
     index.add(embeddings)
     logging.info(f"Model {model_path} loaded and FAISS index created with {index.ntotal} vectors.")
