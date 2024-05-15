de99b2e499b3e2bb9dd286c460eb2bcdcb3e6ee4
JunjieChiang <chunchieh@DESKTOP-RHIRPID>
2024-05-15 18:38:11 +0800

    0515_Jiang

--- generate.py
+++ generate.py
@@ -1,37 +1,11 @@
-import faiss
-from FlagEmbedding import FlagModel
-from http import HTTPStatus
-import json
 import os
-from openai import AzureOpenAI
 import embedding
 from config import get_args
-import logging
 from tqdm import tqdm
+from models import *
 
 
-def setup_openai_client():
-    client = AzureOpenAI(
-        api_key=os.getenv('41df71f980554898b556b2ee3d3dc8d1'),
-        azure_endpoint=os.getenv("https://openai-api-siat.openai.azure.com/"),
-        api_version="2024-02-01"
-    )
-
-    return client
-
-
-def get_completion(client, embedding_model, prompt):
-    response = client.chat.completions.create(
-        model=embedding_model,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ]
-    )
-    return response.choices[0].message.content
-
-
-def generate_questions(client, generative_model, nowsentence, k):
+def generate_questions(llm, nowsentence, k):
     """问题生成"""
     prompt_template = f"""[INST] 
     请根据提供的句子生成{k}个相关的问题。
@@ -48,14 +22,14 @@ def generate_questions(client, generative_model, nowsentence, k):
     2. 回答不允许出现句子中没有的意思
     3. 回答使用中文
     [/INST]"""
-    questions = get_completion(client, generative_model, prompt_template)
+
+    questions = llm.get_completion(prompt_template)
 
     return questions
 
 
-def generate_answers(client, generative_model, questions, relevant_source, k):
+def generate_answers(llm, questions, relevant_source, k):
     """根据问题，生成答案"""
-    # questions_text = '\n'.join(questions)
     prompt_template = f'''
       根据下面的{k}个问题，生成对应的{k}个回答，并根据这些问题和回答构成{k}个JSON对象，每个JSON对象包含以下3个键：
       - "query": 中文的问题，由我给你提供，其中不需要任何数字编号。
@@ -73,12 +47,13 @@ def generate_answers(client, generative_model, questions, relevant_source, k):
       {{"query": "", "pos":["", ""], "neg":["", ""]}}
       请以这种格式输出{k}个JSON对象，每个对象为一行。
         '''
-    answers = get_completion(client, generative_model, prompt_template)
+
+    answers = llm.get_completion(prompt_template)
 
     return answers
 
 
-def get_relevant_source(questions, corpus, embedding_model, top_k):
+def get_relevant_source(questions, index, corpus, embedding_model, top_k):
     questions_embedding = embedding_model.encode([questions]).astype('float32')
 
     _, similar_indices = index.search(questions_embedding, top_k)
@@ -90,27 +65,34 @@ def get_relevant_source(questions, corpus, embedding_model, top_k):
     return result_merge
 
 
-def data_generate(save_path, client, generative_model, corpus, k, embedding_model, top_k):
-    """制作数据集：根据每个段落生成potential问题及相应答案"""
+def main():
 
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+    args = get_args()
 
-    for sentence in tqdm(corpus, desc="Data Generated"):  # 处理corpus中前10个段落
-        questions = generate_questions(client, generative_model, sentence, k)
-        relevant_source = get_relevant_source(questions, corpus, embedding_model, top_k)
-        answers = generate_answers(client, generative_model, questions, relevant_source, k)
-        with open(os.path.join(save_path, 'finetune.txt'), 'a', encoding='utf-8') as file:
+    if args.model_configs == None:
+        args.model_configs = f"model_configs/{args.generative_model}.json"
+
+    if not os.path.exists(args.data_result):
+        os.makedirs(args.data_result)
+
+    # load resources
+    embedding_model, index, corpus = embedding.load_embedding_model(args.embedding_model, args.dimension,
+                                                                    args.data_path)
+    # load LLM configuration
+    llm = init_model_config(args.model_configs)
+
+    # start to generate data
+    for sentence in tqdm(corpus[4029:5000], desc="Data Generated"):
+        questions = generate_questions(llm, sentence, args.k)
+        relevant_source = get_relevant_source(questions, index, corpus, embedding_model, args.top_k)
+        answers = generate_answers(llm, questions, relevant_source, args.k)
+        with open(os.path.join(args.data_result, 'finetune.txt'), 'a', encoding='utf-8') as file:
             full_text = ''.join(answers)
             file.write(full_text + '\n')
 
 
+
 if __name__ == "__main__":
+
     args = get_args()
-    client = setup_openai_client()
-
-    # 在embedding.py里加载模型，把embedding,index保存在本地，然后在这里写一个函数读取就好
-    embedding_model, index, corpus = embedding.load_embedding_model(args.embedding_model, args.dimension,
-                                                                    args.data_path)
-
-    data_generate(args.data_result, client, args.generate_model, corpus, args.k, embedding_model, args.top_k)
+    main()
--- test.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='姓名')
-parser.add_argument('--family', type=str,help='姓')
-parser.add_argument('--name', type=str,help='名')
-args = parser.parse_args()
-print(args.family)
